5fb8df2d714545f4251e74e6c9bd6d4cc7c918e9
7heWatter <147631009+7heWatter@users.noreply.github.com>
2025-06-03 14:22:45 -0300

    03/06

--- main.js
+++ main.js
@@ -16,3 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-06-10T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
